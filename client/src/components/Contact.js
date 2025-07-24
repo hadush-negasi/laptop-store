0e47bd73d8c77372c5884b57100b0fd786586082
Hadush Negasi <hadush7512@gmail.com>
2025-07-24 20:52:27 +0300

    Fix ESLint useEffect dependency warning

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import { FiMail, FiPhone, FiMapPin } from 'react-icons/fi';
 import '../styles.css';
